c6dc0eb1b5af5ee894a85c41bfa67d33b739bae6
Khoa11111 <khoanguyen04102002@gmail.com>
2024-05-31 10:16:54 +0700

    fix conflig

--- app/src/main/java/com/example/e_commerce_app/ui/RegisterSellerActivity.kt
+++ app/src/main/java/com/example/e_commerce_app/ui/RegisterSellerActivity.kt
@@ -5,21 +5,13 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Base64
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.navigation.findNavController
-import com.example.e_commerce_app.MySharedPreferences.MySharedPreferences
-import com.example.e_commerce_app.R
-import com.example.e_commerce_app.databinding.ActivityLoginSignupBinding
 import com.example.e_commerce_app.databinding.ActivityRegisterSellerBinding
 import com.example.e_commerce_app.model.RegistorSellerData
-import com.example.e_commerce_app.model.Shop
-import com.example.e_commerce_app.model.User
 import com.example.e_commerce_app.util.RetrofitInstance
 import com.example.e_commerce_app.util.Utils
 import kotlinx.coroutines.Dispatchers
@@ -27,9 +19,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
-import java.io.ByteArrayOutputStream
 import java.io.IOException
-import kotlin.math.log
 
 class RegisterSellerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterSellerBinding
@@ -50,26 +40,47 @@ class RegisterSellerActivity : AppCompatActivity() {
         binding = ActivityRegisterSellerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val mySharedPreferences:MySharedPreferences(this)
-
-        binding.imgvImgSeller.setOnClickListener{
+        binding.imgvImgSeller.setOnClickListener {
             imageContract.launch("image/*")
         }
-        binding.btnSignupSeller.setOnClickListener{
-            registerSeller(binding.edtShopName.text.toString(),binding.edtAddressSeller.text.toString(),2,"vanphattk159@gmail.com",binding.edtReasonSeller.text.toString(),base64String)
+        binding.btnSignupSeller.setOnClickListener {
+            registerSeller(
+                binding.edtShopName.text.toString(),
+                binding.edtAddressSeller.text.toString(),
+                2,
+                "vanphattk159@gmail.com",
+                binding.edtReasonSeller.text.toString(),
+                base64String
+            )
         }
-        binding.BtnBack.setOnClickListener{
+        binding.BtnBack.setOnClickListener {
             val intent = Intent(applicationContext, HomeActivity::class.java)
             startActivity(intent)
         }
-//        Log.d("Base641", "onCreate: ${base64Strings}")
     }
 
 
-    fun registerSeller(shop_name: String, Address: String, id_user: Int, email_user: String,reason:String,image_shop: String) {
+    fun registerSeller(
+        shop_name: String,
+        Address: String,
+        id_user: Int,
+        email_user: String,
+        reason: String,
+        image_shop: String
+    ) {
         GlobalScope.launch(Dispatchers.IO) {
             val response = try {
-                val registorSellerData = RegistorSellerData(null, shop_name, Address, id_user.toString(), reason, email_user,image_shop, null, null)
+                val registorSellerData = RegistorSellerData(
+                    null,
+                    shop_name,
+                    Address,
+                    id_user.toString(),
+                    reason,
+                    email_user,
+                    image_shop,
+                    null,
+                    null
+                )
 
                 RetrofitInstance.UserApi.RegSeller(registorSellerData)
             } catch (e: HttpException) {
@@ -84,8 +95,10 @@ class RegisterSellerActivity : AppCompatActivity() {
                 if (response.body()!!.err.toString() == "1") {
                     val alertDialog = AlertDialog.Builder(this@RegisterSellerActivity)
                         .setTitle("Register Fail!")
-                        .setMessage("\n" +
-                                "You have already registered, please wait for approval from admin")
+                        .setMessage(
+                            "\n" +
+                                    "You have already registered, please wait for approval from admin"
+                        )
                         .setPositiveButton("OK") { dialog, which ->
                             // Handle positive button click
                             dialog.dismiss()
@@ -97,7 +110,7 @@ class RegisterSellerActivity : AppCompatActivity() {
                         .create()
 
                     alertDialog.show()
-                }else{
+                } else {
                     withContext(Dispatchers.Main) {
                         val intent = Intent(applicationContext, RGSellerOtpActivity::class.java)
                         startActivity(intent)
@@ -106,13 +119,7 @@ class RegisterSellerActivity : AppCompatActivity() {
                 // This block executes only if the response is successful and has a body
             }
         }
-        inputStream.close()
-        val imageBytes = byteArrayOutputStream.toByteArray()
-        return Base64.encodeToString(imageBytes, Base64.DEFAULT)
     }
-
-
-
 
 
 }
